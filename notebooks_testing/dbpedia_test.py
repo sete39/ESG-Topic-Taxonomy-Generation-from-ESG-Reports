Fixed (????) infoNCE loss calculation

notebooks_testing/dbpedia_test.py
@@ -418,13 +418,13 @@ class ModelWithNCE(Model):
         inputs, target = data
         with tf.GradientTape() as tape:
             similarity_prediction, phrase_prediction = self(inputs, training=True)
-            similarity_prediction_infonce = tf.reshape(similarity_prediction / infoNCE_temprature, shape=(1, -1))
+            similarity_prediction_infonce = tf.reshape(similarity_prediction / infoNCE_temprature, shape=(mini_batch_size, -1))
 
-            infoNCE_loss = tf.nn.softmax_cross_entropy_with_logits(labels=tf.reshape(target[0], shape=(1, -1)), logits=similarity_prediction_infonce)
+            infoNCE_loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=tf.reshape(target, shape=(mini_batch_size, -1)), logits=similarity_prediction_infonce))
             phrase_loss = loss_fn(target[1], phrase_prediction)
             total_loss = infoNCE_loss + phrase_loss + sum(self.losses)
 
-            tf.print(infoNCE_loss, total_loss, output_stream=sys.stderr)
+            tf.print(similarity_prediction[:4], infoNCE_loss, total_loss, output_stream=sys.stderr)
         gradients = tape.gradient(total_loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
         self.compiled_metrics.update_state(target, (similarity_prediction, phrase_prediction))